74fc9809f6ad7eb79a323a1c9373db9bd8132a43
Niklas Pieper <pieper.niklas.gm@gmail.com>
2019-09-07 18:55:08 +0200

    Add all data to read.

--- backend/InitializeUser.py
+++ backend/InitializeUser.py
@@ -6,9 +6,31 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        text = input('New data:')
+        name = input('Name:')
         print("Now place your tag to write")
-        reader.write(text)
+        reader.write(name)
         print("Written")
+
+        vorname = input('Vorname:')
+        bday = input('Geburtsdatum:')
+        geburtsort = input('Geburtsort:')
+        nationalitaet = input('Nationalitaet:')
+        beruf = input('Beruf:')
+        strasse = input('Strasse:')
+        plz = input('Plz:')
+        telefon = input('Telefon:')
+        email = input('Email:')
+
+        conn = sqlite3.connect('timetracking.db')
+        c  = conn.cursor()
+
+        c.execute("INSERT INTO user VALUES('{}', '{}', '{}', '{}','{}','{}','{}','{}','{}','{}')".format(id, name, vorname, bday, geburtsort,nationalitaet,beruf,strasse,plz,telefon,email))
+
+        conn.commit()
+        conn.close()
+
+
+
+
 finally:
         GPIO.cleanup()
