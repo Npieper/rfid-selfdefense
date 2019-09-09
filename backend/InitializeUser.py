b9d24240a38225208f058a658670a8cd712e601b
Niklas Pieper <niklas.pieper@trustedshops.com>
2019-09-09 12:04:19 +0200

    Initialize chip only with id, so the other information will be updated later on in frontend.

--- backend/InitializeUser.py
+++ backend/InitializeUser.py
@@ -8,30 +8,18 @@ import sqlite3
 reader = SimpleMFRC522()
 
 try:
-        name = input('Name:')
-        print("Now place your tag to write")
-        reader.write(name)
-        print("Written")
-
-        vorname = input('Vorname:')
-        bday = input('Geburtsdatum:')
-        geburtsort = input('Geburtsort:')
-        nationalitaet = input('Nationalitaet:')
-        beruf = input('Beruf:')
-        strasse = input('Strasse:')
-        plz = input('Plz:')
-        telefon = input('Telefon:')
-        email = input('Email:')
+        id = input('Bitte ID eingeben.')
+        print("Jetzt den Chip auf das Lesegeraet legen.")
+        reader.write(id)
+        print("Chip beschrieben.")
 
         conn = sqlite3.connect('timetracking.db')
         c  = conn.cursor()
 
-        c.execute("INSERT INTO user VALUES('{}', '{}', '{}', '{}','{}','{}','{}','{}','{}','{}','{}')".format(id, name, vorname, bday, geburtsort,nationalitaet,beruf,strasse,plz,telefon,email))
+        c.execute("INSERT INTO user id VALUES('{}')".format(id))
 
         conn.commit()
         conn.close()
-
-
 
 
 finally:
