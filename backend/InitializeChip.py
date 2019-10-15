4a8b1005abafba3f2b6153ecda316df5ca277f6e
Niklas Pieper <pieper.niklas@gmx.de>
2019-10-15 22:41:00 +0000

    Initialize Chip with insert into databse, fix ReadUser and delete timetracking db.

--- backend/InitializeChip.py
+++ backend/InitializeChip.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
+from mfrc522 import SimpleMFRC522
+
 try:
     connection = mysql.connector.connect(host='us-cdbr-iron-east-02.cleardb.net',
                                          database='heroku_f88e5c34407b619',
@@ -9,9 +11,14 @@ try:
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
         cursor = connection.cursor()
+        
+        reader = SimpleMFRC522()
 
+        id = input('Bitte ID eingeben.')
+        print("Jetzt den Chip auf das Lesegeraet legen.")
+        reader.write(id)
+        print("Chip beschrieben.")
 
-        id = input("ID:")
 
         mySql_insert_query = "INSERT INTO user (ChipID) VALUES ('{}')".format(id)
         cursor = connection.cursor()
