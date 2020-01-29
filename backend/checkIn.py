eb74ca9fcd97ea7f3f9142a1f1de4015386ca5cc
Niklas Pieper <niklas.pieper@trustedshops.com>
2020-01-29 14:06:58 +0100

    Refactoring.

--- backend/checkIn.py
+++ backend/checkIn.py
@@ -3,6 +3,24 @@ from mysql.connector import Error
 import datetime
 from datetime import datetime, timedelta
 
+def calculate_week_start_date():
+    today = datetime.now().date()
+    start = today - timedelta(days=today.weekday())
+    end = start + timedelta(days=6)
+    print("Today: " + str(today))
+    print("Start: " + str(start))
+    return start
+
+def is_user_allowed(package_number, number_of_trainings):
+    if package_number == 2:
+        print("Package number is 2")
+        if number_of_trainings > 3:
+            print("WARNING: Client is not allowed to train that often!")
+    elif package_number == 1:
+        print("Package number is 1")
+    elif package_number == 0:
+        print("Package number is 0")
+
 try:
     connection = mysql.connector.connect(
         host='rfid-selfdefense.chekdlwyhdsh.eu-central-1.rds.amazonaws.com',
@@ -13,43 +31,29 @@ try:
 
     if connection.is_connected():
 
-
         # Read in user with chip and save the chip id
         # ...
 
         cursor = connection.cursor(named_tuple=True)
 
-        # Calculate start of the week
-        today = datetime.now().date()
-        start = today - timedelta(days=today.weekday())
-        end = start + timedelta(days=6)
-        print("Today: " + str(today))
-        print("Start: " + str(start))
-        # ---------------------------
+        week_start_day = calculate_week_start_date()
 
-        cursor.execute("SELECT checkIn FROM time where checkIn > '{}'".format(start))
+        cursor.execute("SELECT checkIn FROM time where checkIn > '{}'".format(week_start_day))
 
-        countRows = 0        
+        count_rows = 0        
         for x in cursor:
             print(x.checkIn)
-            countRows += 1
+            count_rows += 1
 
-        print(countRows) 
+        print(count_rows) 
 
         cursor.execute("SELECT package FROM user where ChipId = '{}'".format(1))
         
-        packageNumber = -1
+        package_number = -1
         for y in cursor:
-            packageNumber = y.package
-
-        if packageNumber == 2:
-            print("Package number is 2")
-            if countRows > 3:
-                print("WARNING: Client is not allowed to train that often!")
-        elif packageNumber == 1:
-            print("Package number is 1")
-        elif packageNumber == 0:
-            print("Package number is 0")
+            package_number = y.package
+            
+        is_user_allowed(package_number, count_rows)
            
         # Have a look at the timezones, does not make sense right now    
         mySql_insert_query = "INSERT INTO time (userId, checkIn) VALUES ('{}', '{}')".format(1, datetime.now() + timedelta(hours=-1))
